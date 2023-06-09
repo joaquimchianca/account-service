f672e7a27c019369d4df726b52668bf46ddc2183
Joaquim Chianca <joaquimchianca@gmail.com>
2023-06-09 03:15:32 -0300

    criado repository

---
+++ src/main/java/ufrn/imd/userservice/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package ufrn.imd.userservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ufrn.imd.userservice.model.Usuario;
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
