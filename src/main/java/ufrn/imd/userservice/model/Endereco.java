32bb2adf2625f9de3ec662809b03c339b8cea37f
Joaquim Chianca <joaquimchianca@gmail.com>
2023-06-09 03:16:06 -0300

    adicionando construtores

--- src/main/java/ufrn/imd/userservice/model/Endereco.java
+++ src/main/java/ufrn/imd/userservice/model/Endereco.java
@@ -1,11 +1,15 @@
-package ufrn.imd.accountservice.models;
+package ufrn.imd.userservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "endereco")
 public class Endereco {
