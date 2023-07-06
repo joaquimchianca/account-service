232d7ecc4fea506c5bf934d3ab7e691a8066f1c0
Joaquim Chianca <joaquimchianca@gmail.com>
2023-07-05 21:01:06 -0300

    faet: annotation para permitir o rabbit

--- src/main/java/ufrn/imd/userservice/UserServiceApplication.java
+++ src/main/java/ufrn/imd/userservice/UserServiceApplication.java
@@ -1,9 +1,11 @@
 package ufrn.imd.userservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
